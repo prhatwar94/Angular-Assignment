e02a037c658dc41c25489575b31dff6009017e2c
Prashant Hatwar <prashant.hatwar@publicissapient.com>
2021-10-20 21:25:15 +0530

    Dynamic div component changes

--- src/app/dynamic-div/dynamic-div.component.ts
+++ src/app/dynamic-div/dynamic-div.component.ts
@@ -1,38 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-export interface Numbers{
-  number:Number
-}
+import { AfterViewInit, Component, ContentChild, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+
 @Component({
   selector: 'app-dynamic-div',
   templateUrl: './dynamic-div.component.html',
   styleUrls: ['./dynamic-div.component.css']
 })
 export class DynamicDivComponent implements OnInit {
-
-// numbers = Array.from({length: 100000}).map((_, i) => i);
-post:Array<number>[]=[];
-numbers:any=[];
-  constructor() { }
+  @ViewChild('dynamicDiv')  
+  dynamicDiv!: ElementRef;
+  constructor(private elRef:ElementRef,private renderer: Renderer2) { }
+  
 
 
   ngOnInit(): void {
-    for(let i:number=1;i<=100;i++){
-      this.numbers.push(i);
-    }
+   for(let i:number=1;i<=100;i++){
+      this.createDivContainer(i);
+   }
   }
+ 
 
   onButtonClick(number:Number){
-    alert("Button in "+number+" div  clicked ");
-   
+    alert("Button in "+number+" div  clicked ");  
   }
+  
 
-  onScroll(){
-    const length=this.numbers.length;
-    setTimeout(() => {
-      const p:any=' '.repeat(100).split('').map((s,i)=>i+1+length);
-        while(p.length)
-        this.numbers.push(p.shift());
-    }, 100);
+  createDivContainer(i:number) {
+    const buttonEle=this.renderer.createElement('button');
+    const text=this.renderer.createText('Button '+i);
+    this.renderer.appendChild(buttonEle,text);
+     const dynamicDivContainer=this.elRef.nativeElement.querySelector('div')
+    const divContainer = this.renderer.createElement('div');
+    this.renderer.appendChild(dynamicDivContainer, divContainer);   
+    this.renderer.setAttribute(divContainer, 'class',  'number');
+    this.renderer.appendChild(divContainer,buttonEle);
+    this.renderer.listen(buttonEle, 'click', (event) => {
+      this.onButtonClick(i);
+   })
+    return divContainer;
   }
-
 }
